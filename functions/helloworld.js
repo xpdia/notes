b2c072988e3fad21aecaed208099b64e27cca094
xpdia <153821585+xpdia@users.noreply.github.com>
2024-01-30 10:20:30 +0530

    Update helloworld.js

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,35 +1,3 @@
-const { brotliCompress } = require('zlib');
-const { URL } = require('url');
-const { parse } = require('querystring');
-
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
-  const url = new URL(request.url);
-  const query = parse(url.search.slice(1));
-
-  if (query.text) {
-    const compressedText = await compressText(query.text);
-    return new Response(JSON.stringify({ compressedText }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } else {
-    return new Response('Please provide a "text" query parameter.', {
-      status: 400,
-    });
-  }
-}
-
-async function compressText(text) {
-  return new Promise((resolve, reject) => {
-    brotliCompress(text, (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result.toString('base64'));
-      }
-    });
-  });
+export function onRequest(context) {
+  return new Response("true")
 }
